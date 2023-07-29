c6eee4ee46de71390be557b696340c966bbc4cd0
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-07-29 16:00:21 +0300

    add_route_to_Navbar_les19

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import {Header} from "./Components/Header/Header";
 import {Navbar} from "./Components/Navbar/Navbar";
 import {Profile} from "./Components/Profile/Profile";
 import {Dialogs} from "./Components/Dialogs/Dialogs";
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App() {
+
     return (
         <div className="App">
             <Header/>
             <Navbar/>
             <div className={'content'}>
-                <Profile/>
-                <Dialogs/>
+                <Routes>
+                    <Route path={'/profile'} element={<Profile/>}></Route>
+                    <Route path={'/messages'} element={<Dialogs/>}></Route>
+                </Routes>
             </div>
             <div className={'promo'}>
                 <p>Games</p>
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
+
     return (
         <div className={s.navbar}>
             <img src="https://img.icons8.com/?size=512&id=65220&format=png" alt=""/>
-            <a href="#">Profile</a>
-            <a href="#">Messages</a>
-            <a href="#">News</a>
-            <a href="#">Music</a>
+            <NavLink to="/profile">Profile</NavLink>
+            <NavLink to="/messages">Messages</NavLink>
+            <NavLink to="/news">News</NavLink>
+            <NavLink to="/music">Music</NavLink>
         </div>
     );
 };
