93dd41696d0168def1486fe49b8c6e7a9dc6fdde
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-07-29 14:04:09 +0300

    add_wrapper_to_content_lesson_18

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import './App.css';
 import {Header} from "./Components/Header/Header";
 import {Navbar} from "./Components/Navbar/Navbar";
 import {Profile} from "./Components/Profile/Profile";
+import {Dialogs} from "./Components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className={'content'}>
+                <Profile/>
+                <Dialogs/>
+            </div>
             <div className={'promo'}>
                 <p>Games</p>
                 <p>Features</p>
