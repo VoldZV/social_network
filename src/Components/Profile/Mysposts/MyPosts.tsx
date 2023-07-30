0b97c83ba90b976df6c3ce483b163a1e500fe307
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-07-31 00:27:49 +0300

    refactor_MyPosts_component_transfer_data_from_JSX

--- src/Components/Profile/Mysposts/MyPosts.tsx
+++ src/Components/Profile/Mysposts/MyPosts.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
+    const postsData = [
+        {id: 1, message: 'First post message', likesCount: 7},
+        {id: 2, message: 'Second post message', likesCount: 1},
+        {id: 3, message: 'Third post message', likesCount: 3},
+    ]
+    const posts = postsData.map(post => <Post key={post.id} message={post.message} likesCount={post.likesCount}/>)
+
     return (
         <>
             <div>
@@ -9,9 +16,7 @@ export const MyPosts = () => {
                 <textarea name="addPost" id="area" cols={50} rows={5}></textarea>
                 <button>Add Post</button>
             </div>
-            <Post message={'First post message'} likesCount={7}/>
-            <Post message={'Second post message'} likesCount={1}/>
-            <Post message={'Third post message'} likesCount={3}/>
+            {posts}
         </>
     );
 };
